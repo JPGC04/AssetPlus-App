3db0468f98ef4955c86bcaf603a23490cfeb8f7c
justsom1-nizar <116989494+justsom1-nizar@users.noreply.github.com>
2023-10-17 02:08:38 -0400

    Update AssetPlusFeatureSet7Controller.java

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet7Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet7Controller.java
@@ -1,26 +1,47 @@
 package ca.mcgill.ecse.assetplus.controller;
 
 import java.sql.Date;
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.AssetPlus;
+import ca.mcgill.ecse.assetplus.model.MaintenanceNote;
+import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
 
 public class AssetPlusFeatureSet7Controller {
+  private static AssetPlus assetplus=AssetPlusApplication.getAssetPlus();
+
+  private AssetPlusFeatureSet7Controller() {}
+
 
   public static String addMaintenanceNote(Date date, String description, int ticketID,
       String email) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    MaintenanceTicket ticket=Utils.getMaintenanceTicketbyID(ticketID);
+    if(ticket!=null){
+      ticket.addTicketNote(new MaintenanceNote(date,description,ticket,Utils.getHotelStaffbyemail(email)));
+      return "";
+    }
+    return "error";
   }
 
   // index starts at 0
   public static String updateMaintenanceNote(int ticketID, int index, Date newDate,
       String newDescription, String newEmail) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    MaintenanceNote maintenanceNote=Utils.getMaintenanceTicketbyID(ticketID).getTicketNotes().get(index);
+    if(maintenanceNote!=null){
+      maintenanceNote.setDate(newDate);
+      maintenanceNote.setNoteTaker(Utils.getHotelStaffbyemail(newEmail));
+      maintenanceNote.setDescription(newDescription);
+      return "";
+    }
+    return "error";
   }
 
   // index starts at 0
   public static void deleteMaintenanceNote(int ticketID, int index) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    MaintenanceNote maintenanceNote=Utils.getMaintenanceTicketbyID(ticketID).getTicketNotes().get(index);
+    if(maintenanceNote!=null){
+      maintenanceNote.delete();
+    }
+    throw new RuntimeException("Ticket/Note not found.");
   }
 
 }
