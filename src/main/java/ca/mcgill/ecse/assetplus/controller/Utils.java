ec6bab39fb35f8fe9524371aa1cb36caa3325900
justsom1-nizar <116989494+justsom1-nizar@users.noreply.github.com>
2023-10-17 02:09:13 -0400

    Update Utils.java

--- src/main/java/ca/mcgill/ecse/assetplus/controller/Utils.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/Utils.java
@@ -1,4 +1,31 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.AssetPlus;
+import ca.mcgill.ecse.assetplus.model.HotelStaff;
+import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
+
 public class Utils {
+    private static AssetPlus assetplus= AssetPlusApplication.getAssetPlus();
+
+    private Utils() {}
+    public static MaintenanceTicket getMaintenanceTicketbyID(int ticketID){
+        for (MaintenanceTicket ticket:assetplus.getMaintenanceTickets()) {
+            if (ticketID==ticket.getId()) {
+                return ticket;
+            }
+        }
+        return null;
+    }
+    public static HotelStaff getHotelStaffbyemail(String email){
+        if(assetplus.getManager().getEmail()==email){
+            return assetplus.getManager();
+        }
+        for (HotelStaff staff:assetplus.getEmployees()) {
+            if (email==staff.getEmail()) {
+                return staff;
+            }
+        }
+        return null;
+    }
 }
