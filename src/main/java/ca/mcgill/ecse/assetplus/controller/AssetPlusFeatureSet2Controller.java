08af5a40d79eea4a8605fcea5ce4172adf4d999b
John-Paul <john-paul.chouery@mail.mcgill.ca>
2023-10-17 22:55:44 -0400

    implemented AssetType (2) on my own branch JP

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
@@ -1,21 +1,51 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.AssetType;
+
 public class AssetPlusFeatureSet2Controller {
 
   public static String addAssetType(String name, int expectedLifeSpanInDays) {
+    if (name == null) {
+      throw new IllegalArgumentException("Enter approporaite name");
+    }
+    if (expectedLifeSpanInDays <= null) {
+      throw new IllegalArgumentException("Enter approporaite Life Span");
+    }
+
+    AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
+    assetPlus.assetType(name, expectedLifeSpanInDays, assetPlus);
+    
     // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
-    // modified I should be in my own branch now
+    // throw new UnsupportedOperationException("Not Implemented!");
   }
 
   public static String updateAssetType(String oldName, String newName, int newExpectedLifeSpanInDays) {
+    if (oldName == null) {
+      throw new IllegalArgumentException("Enter approporaite old name");
+    }
+    if (oldName == null) {
+      throw new IllegalArgumentException("Enter approporaite new name");
+    }
+    if (newExpectedLifeSpanInDays == null) {
+      throw new IllegalArgumentException("Enter approporaite expected lifespan");
+    }
+
+    oldName.setName(newName);
+    newName.setExpectedLifeSpan(newExpectedLifeSpanInDays);
+
     // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    // throw new UnsupportedOperationException("Not Implemented!");
   }
 
   public static void deleteAssetType(String name) {
+    if (name == null) {
+      throw new IllegalArgumentException("Enter approporaite name");
+    }
+    name.delete();
+
     // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    // throw new UnsupportedOperationException("Not Implemented!");
   }
 
 }
