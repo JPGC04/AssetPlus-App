a8d346e73b6a0a080c5be0fdad9b62d544b69953
justsom1-nizar <116989494+justsom1-nizar@users.noreply.github.com>
2023-10-19 14:58:52 -0400

    Update AssetPlusFeatureSet5Controller.java

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -1,46 +1,47 @@
 package ca.mcgill.ecse.assetplus.controller;
 
-import java.util.List;
 
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
 import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
 import ca.mcgill.ecse.assetplus.model.TicketImage;
 
+import java.util.List;
+
 /**
  * AssetPlusFeatureSet5Controller is a set of methods that add and delete images to maintenance tickets
- * 
+ *
  * @author Alan Brotherton -- AlanBrotherton
  * @version ECSE 223 - Group Project Iteration 2a
  * @since ECSE 223 - Group Project Iteration 2a
  */
 
 public class AssetPlusFeatureSet5Controller {
-	
-private static AssetPlus assetplus = AssetPlusApplication.getAssetPlus();
+
+  private static AssetPlus assetplus = AssetPlusApplication.getAssetPlus();
 
   public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
-	  
-	  if (imageURL == null || imageURL.isEmpty() ) {
-		  return "Image URL cannot be empty";
-	  }
-	  
-	  if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
-		  return "Image URL must start with http:// or https://";
-	  }
-	  
-    MaintenanceTicket maintenanceTicket = Utils.getMaintenanceTicketbyID(ticketID);
+
+    if (imageURL == null || imageURL.isEmpty() ) {
+      return "Image URL cannot be empty";
+    }
+
+    if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
+      return "Image URL must start with http:// or https://";
+    }
+
+    MaintenanceTicket maintenanceTicket = MaintenanceTicket.getWithId(ticketID);
     if (maintenanceTicket != null) {
-    	
-    	List<TicketImage> ticketImages = maintenanceTicket.getTicketImages();
-    	for (TicketImage image : ticketImages) {
-          if (imageURL.equals(image.getImageURL())) {
-               return "Image already exists for the ticket";
-            }
+
+      List<TicketImage> ticketImages = maintenanceTicket.getTicketImages();
+      for (TicketImage image : ticketImages) {
+        if (imageURL.equals(image.getImageURL())) {
+          return "Image already exists for the ticket";
         }
-    	
-    	maintenanceTicket.addTicketImage(imageURL);
-      
+      }
+
+      maintenanceTicket.addTicketImage(imageURL);
+
       return "";
     }
     else return "Ticket does not exist";
@@ -51,10 +52,10 @@ private static AssetPlus assetplus = AssetPlusApplication.getAssetPlus();
     if(maintenanceTicket != null){
       List<TicketImage> ticketImages = maintenanceTicket.getTicketImages();
       for (TicketImage image : ticketImages) {
-        if(imageURL == image.getImageURL()){
+        if(imageURL.equals(image.getImageURL())){
           maintenanceTicket.removeTicketImage(image);
         }
       }
-    } 
+    }
   }
 }
