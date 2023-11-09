d6b7f34aadc4531a3b71e7e35fbe4a40fa16cbd3
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2023-11-08 22:45:13 -0500

    Update getAssetPlus() with persistence package

--- src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
+++ src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
@@ -8,12 +8,13 @@ public class AssetPlusApplication {
   private static AssetPlus assetPlus;
 
   public static void main(String[] args) {
+    assetPlus = getAssetPlus();
     // TODO Start the application user interface here
   }
 
   public static AssetPlus getAssetPlus() {
     if (assetPlus == null) {
-      assetPlus = new AssetPlus();
+      assetPlus = AssetPlusPersistence.load();
     }
     return assetPlus;
   }
