16b9319bd9af2814e6ab72aae3866b9059422667
Dramocrystal <69166231+Dramocrystal@users.noreply.github.com>
2023-11-27 14:57:24 -0500

    Update AssetPlusApplication.java

--- src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
+++ src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
@@ -2,14 +2,26 @@ package ca.mcgill.ecse.assetplus.application;
 
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
 import ca.mcgill.ecse.assetplus.persistence.AssetPlusPersistence;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class AssetPlusApplication {
+
+
+public class AssetPlusApplication extends Application {
 
   private static AssetPlus assetPlus;
 
   public static void main(String[] args) {
     assetPlus = getAssetPlus();
-    // TODO Start the application user interface here
+    // To do Start the application user interface here
+    launch(args);
+    // test
+
   }
 
   public static AssetPlus getAssetPlus() {
@@ -19,4 +31,25 @@ public class AssetPlusApplication {
     return assetPlus;
   }
 
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Hello World!");
+    Button btn = new Button();
+    btn.setText("Say 'Hello World'");
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+      @Override
+      public void handle(ActionEvent event) {
+        System.out.println("Hello World!");
+      }
+
+    });
+
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+    primaryStage.setScene(new Scene(root, 300, 250));
+    primaryStage.show();
+  }
+
 }
+
