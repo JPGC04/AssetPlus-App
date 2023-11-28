bc22a85b138b452f39ab150b90951f976bdef05f
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2023-11-27 21:01:32 -0500

    Working UI with my non-functioning controller

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AUDAssetTypeController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AUDAssetTypeController.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
+import static ca.mcgill.ecse.assetplus.javafx.fxml.controllers.ViewUtils.successful;
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet2Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -25,17 +27,26 @@ public class AUDAssetTypeController {
 
     @FXML
     void addNewAssetType(ActionEvent event) {
+        String name = newAssetTypeName.getText();
+        if (name == null || name.trim().isEmpty()) {
+            ViewUtils.showError("Please input a valid driver name");
+        } else {
+            // reset the driver text field if success
+            if (successful(AssetPlusFeatureSet2Controller.addAssetType(name, 4))) {
+                newAssetTypeName.setText("");
+            }
+        }
 
     }
 
     @FXML
     void deleteNewAssetType(ActionEvent event) {
-
+        return;
     }
 
     @FXML
     void updateNewAssetType(ActionEvent event) {
-
+        return;
     }
 
 }
