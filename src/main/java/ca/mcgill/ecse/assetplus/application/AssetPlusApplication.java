73e7acd53aa6026060e65be14e7aeb93d2565961
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2023-11-27 20:30:43 -0500

    Restored file to original state, tests pass

--- src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
+++ src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
@@ -2,26 +2,14 @@ package ca.mcgill.ecse.assetplus.application;
 
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
 import ca.mcgill.ecse.assetplus.persistence.AssetPlusPersistence;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
-
-
-public class AssetPlusApplication extends Application {
+public class AssetPlusApplication {
 
   private static AssetPlus assetPlus;
 
   public static void main(String[] args) {
     assetPlus = getAssetPlus();
-    // To do Start the application user interface here
-    launch(args);
-    // test
-
+    // TODO Start the application user interface here
   }
 
   public static AssetPlus getAssetPlus() {
@@ -31,25 +19,4 @@ public class AssetPlusApplication extends Application {
     return assetPlus;
   }
 
-  @Override
-  public void start(Stage primaryStage) {
-    primaryStage.setTitle("Hello World!");
-    Button btn = new Button();
-    btn.setText("Say 'Hello World'");
-    btn.setOnAction(new EventHandler<ActionEvent>() {
-
-      @Override
-      public void handle(ActionEvent event) {
-        System.out.println("Hello World!");
-      }
-
-    });
-
-    StackPane root = new StackPane();
-    root.getChildren().add(btn);
-    primaryStage.setScene(new Scene(root, 300, 250));
-    primaryStage.show();
-  }
-
 }
-
