df37bd6f640c880ba17a06220bd480c674abfbd5
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2023-11-27 20:43:27 -0500

    Working ui with only addelementpage

--- src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
+++ src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
@@ -1,15 +1,18 @@
 package ca.mcgill.ecse.assetplus.application;
 
+import ca.mcgill.ecse.assetplus.javafx.fxml.AssetPlusFxmlView;
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
 import ca.mcgill.ecse.assetplus.persistence.AssetPlusPersistence;
+import javafx.application.Application;
 
 public class AssetPlusApplication {
 
   private static AssetPlus assetPlus;
 
   public static void main(String[] args) {
-    assetPlus = getAssetPlus();
+    assetPlus = getAssetPlus(); // might remove this line
     // TODO Start the application user interface here
+    Application.launch(AssetPlusFxmlView.class, args);
   }
 
   public static AssetPlus getAssetPlus() {
