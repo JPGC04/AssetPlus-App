3f5897fd909abc168a3bcafd175d60bf54b8c564
plain-noodle-expert <114117134+plain-noodle-expert@users.noreply.github.com>
2023-11-27 22:47:00 -0500

    Update ViewStatusMTicketController.java

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ViewStatusMTicketController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ViewStatusMTicketController.java
@@ -1,1 +1,109 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class ViewStatusMTicketController {
+
+    @FXML
+    private TextField assetTextField;
+
+    @FXML
+    private TextField descriptionTextField;
+
+    @FXML
+    private TextField endDateTextField;
+
+    @FXML
+    private Button imageButton;
+
+    @FXML
+    private CheckBox managerApprovalCheckBox;
+
+    @FXML
+    private TextField priorityLevelTextField;
+
+    @FXML
+    private TextField startDateTextField;
+
+    @FXML
+    private TextField statusTextField;
+
+    @FXML
+    private DatePicker ticketDatePicker;
+
+    @FXML
+    private ComboBox<?> ticketNumberSearchComboBox;
+
+    @FXML
+    private Button viewLocationButton;
+
+    @FXML
+    private Button viewNotesButton;
+
+    @FXML
+    void getAsset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getDescription(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getEndDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getImages(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getLocation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getNotes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getPriorityLevel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getStartDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getTicketOnDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getTicketWithNumber(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ifManagerApproval(ActionEvent event) {
+
+    }
+
+}
