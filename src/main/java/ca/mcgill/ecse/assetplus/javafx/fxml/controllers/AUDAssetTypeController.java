512a70551d5dd0639f5fa2289285dbaf0eed37d9
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2023-11-29 16:51:06 -0500

    new audassettype with controller

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AUDAssetTypeController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AUDAssetTypeController.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+/*package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
 import static ca.mcgill.ecse.assetplus.javafx.fxml.controllers.ViewUtils.successful;
 import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet2Controller;
@@ -47,6 +47,59 @@ public class AUDAssetTypeController {
     @FXML
     void updateNewAssetType(ActionEvent event) {
         return;
+    }
+
+}*/
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class AUDAssetTypeController {
+
+    @FXML
+    private Button AddAssetTypeButton;
+
+    @FXML
+    private TextField AddAssetTypeLife;
+
+    @FXML
+    private TextField AddAssetTypeName;
+
+    @FXML
+    private Button DeleteAssetTypeButton;
+
+    @FXML
+    private ChoiceBox<?> DeleteAssetTypeName;
+
+    @FXML
+    private Button UpdateAssetTypeButton;
+
+    @FXML
+    private TextField UpdateAssetTypeNewLife;
+
+    @FXML
+    private TextField UpdateAssetTypeNewName;
+
+    @FXML
+    private ChoiceBox<?> UpdateAssetTypeOldName;
+
+    @FXML
+    void AddAssetTypeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteAssetTypeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UpdateAssetTypeClicked(ActionEvent event) {
+
     }
 
 }
