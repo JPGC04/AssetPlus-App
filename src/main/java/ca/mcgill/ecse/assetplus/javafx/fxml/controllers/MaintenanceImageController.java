ca867a271b82675b02027412d0c56c459ab7183a
Dramocrystal <asselahyazid@gmail.com>
2023-11-30 12:06:52 -0500

    New MaintenanceImage UI

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/MaintenanceImageController.java
@@ -0,0 +1,55 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class MaintenanceImageController {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField dateInput;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private TextField imageURLinput;
+
+    @FXML
+    private TableView<?> images;
+
+    @FXML
+    private TextField ticketInput;
+
+    @FXML
+    private TableColumn<?, ?> urlTable;
+
+    @FXML
+    void createClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rowClicked(MouseEvent event) {
+
+    }
+
+}
