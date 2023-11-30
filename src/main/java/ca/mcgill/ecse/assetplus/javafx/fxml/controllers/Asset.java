75faa886fc6a79f72d84ecd534d5ba6bbd852f75
Seungyeon Lee <seungyeon.lee3@mail.mcgill.ca>
2023-11-30 00:03:09 -0500

    asset class for AUDAssetController.java (V1 of asset page)

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/Asset.java
@@ -0,0 +1,64 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import javafx.scene.control.Button;
+
+public class Asset {
+
+  private String ID;
+  private String date;
+  private String floor;
+  private String room;
+  private String assetType;
+
+
+  public Asset(String ID, String date, String floor, String room, String assetType) {
+    this.ID = ID;
+    this.date = date;
+    this.floor = floor;
+    this.room = room;
+    this.assetType = assetType;
+
+  }
+
+  public String getID() {
+    return ID;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public String getFloor() {
+    return floor;
+  }
+
+  public String getRoom() {
+    return room;
+  }
+
+  public String getAssetType() {
+    return assetType;
+  }
+
+
+  public void setID(String ID) {
+    this.ID = ID;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public void setFloor(String floor) {
+    this.floor = floor;
+  }
+
+  public void setRoom(String room) {
+    this.room = room;
+  }
+
+  public void setAssetType(String assetType) {
+    this.assetType = assetType;
+  }
+
+}
