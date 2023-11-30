14ed6c98e155caeb7c97e41f3216c1b656b8ac3e
Seungyeon Lee <seungyeon.lee3@mail.mcgill.ca>
2023-11-30 00:00:58 -0500

    all functionality for asset page working (controller)

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AUDAssetController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AUDAssetController.java
@@ -1,114 +1,168 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
 import static ca.mcgill.ecse.assetplus.javafx.fxml.controllers.ViewUtils.showError;
+import java.net.URL;
 import java.sql.Date;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+
 import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet3Controller;
 
-public class AUDAssetController {
+public class AUDAssetController implements Initializable{
+    @FXML
+    private TableColumn<Asset, String> assetCol;
 
     @FXML
-    private TextField textArea;
+    private TextField assetID;
 
     @FXML
-    public void initialize() {
+    private TextField assetType;
 
-    }
+    @FXML
+    private TableColumn<Asset, String> assetTypeCol;
+
+    @FXML
+    private TableColumn<Asset, String> floorCol;
+
+    @FXML
+    private TextField floorNumber;
+
+    @FXML
+    private TableColumn<Asset, String> purchaseCol;
+
+    @FXML
+    private DatePicker purchaseDate;
+
+    @FXML
+    private TableColumn<Asset, String> roomCol;
+
+    @FXML
+    private TextField roomNumber;
+
     @FXML
     private VBox screen;
 
-    private List<HBox> popup= new ArrayList<>();
+    @FXML
+    private TableView<Asset> table;
 
-    private void addAsset(TextField purchaseDate, TextField floorNumber, TextField roomNumber, TextField assetType, String assetID) {
-        //add asset
-        //except error
+    @FXML
+    private TextField textArea;
+    
+    private ObservableList<Asset> data;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        assetCol.setCellValueFactory(new PropertyValueFactory<Asset, String>("ID"));
+        floorCol.setCellValueFactory(new PropertyValueFactory<Asset, String>("floor"));
+        roomCol.setCellValueFactory(new PropertyValueFactory<Asset, String>("room"));
+        purchaseCol.setCellValueFactory(new PropertyValueFactory<Asset, String>("date"));
+        assetTypeCol.setCellValueFactory(new PropertyValueFactory<Asset, String>("assetType"));
+
+
+        data = FXCollections.observableArrayList();
+        table.setItems(data);
     }
 
-    private void deleteAsset(int i, String assetID) {
-        //delete button from database
-        //if successful
+    @FXML
+    void onAddBtnClick(ActionEvent event) {
         try {
-            int assetNumber = Integer.parseInt(assetID);
-            AssetPlusFeatureSet3Controller.deleteSpecificAsset(assetNumber);
-        } catch (Exception e) {
-            showError("Invalid parameters");
-        }
-        screen.getChildren().remove(popup.get(i));
-    }
-
-    private void updateAsset(int i, TextField purchaseDate, TextField floorNumber, TextField roomNumber, TextField assetType, String assetID) {
-        //update asset
-        //if succesful
-        try {
+            int id = Integer.parseInt(assetID.getText());
+            LocalDate x = purchaseDate.getValue();
+            Date date = java.sql.Date.valueOf(x);
             int floorNo = Integer.parseInt(floorNumber.getText());
             int roomNo = Integer.parseInt(roomNumber.getText());
-            Date newPurchaseDate = Date.valueOf(purchaseDate.getText());
-            String newAssetType = assetType.getText();
-            int assetNumber = Integer.parseInt(assetID);
-            String error = AssetPlusFeatureSet3Controller.updateSpecificAsset(assetNumber, floorNo, roomNo, newPurchaseDate, newAssetType);
-            if (error.isBlank()) {
-                screen.getChildren().remove(popup.get(i));
+            String aType = assetType.getText(); 
+            String err = AssetPlusFeatureSet3Controller.addSpecificAsset(id, floorNo, roomNo, date, aType);
+            System.out.println(err);
+            if (!err.isEmpty()) {
+                data.add(new Asset(assetID.getText(), String.valueOf(date), floorNumber.getText(), roomNumber.getText(), aType));
+                showError("Success!");
+                assetID.clear();
+                floorNumber.clear();
+                roomNumber.clear();
+                assetType.clear();
+            } else {
+                showError(err);
             }
         } catch (Exception e) {
-            //popup screen that says invalid parameters
-            showError("Invalid parameters.");
-
+            showError("Invalid Parameters");
         }
         
-   
     }
 
-    private HBox createPreConfiguredPane() {
-        // Create or load your pre-configured Pane
-        // For example, create a Pane with labels
-        HBox newBox = new HBox();
-        Label assetID = new Label("Label 1");
-        assetID.setText("this is label1");
-        String idString = "";
-        TextField purchaseDate = new TextField();
-        TextField floorNumber = new TextField();
-        TextField roomNumber = new TextField();
-        TextField assetType = new TextField();
-        Button delete = new Button();
-        delete.setText("X");
-        delete.setStyle("-fx-background-color: #ff0000; -fx-text-fill: #000000; -fx-font-size: 14px;");
-        Button update = new Button();
-        update.setText("Update");
-        update.setStyle("-fx-background-color: #fffb00; -fx-text-fill: #000000; -fx-font-size: 14px;");
-        
-        int index = popup.size();
-        delete.setOnAction(e -> deleteAsset(index, idString));
-        update.setOnAction(e -> updateAsset(index, purchaseDate, floorNumber, roomNumber, assetType, idString));
 
-        popup.add(newBox);
-        // Add labels and other nodes to the Pane as needed
-        newBox.getChildren().addAll(assetID, purchaseDate, floorNumber, roomNumber, assetType, delete, update);
-
-        return newBox;
-    }
     @FXML
-    protected void onTextButtonClick() {
-        String content = textArea.getText();
-        textArea.clear();
-
-        Label one = new Label("HELOOOOOOOOOOOOOO");
-        one.setText("IIIIIIIIIIIIIIIII");
-        HBox newBox = createPreConfiguredPane();
-        
-        if (!popup.isEmpty()) {
-            screen.getChildren().add(newBox);
+    public void onUpdateBtnClick(ActionEvent event) {
+        try {
+            int id = Integer.parseInt(assetID.getText());
+            LocalDate x = purchaseDate.getValue();
+            Date date = java.sql.Date.valueOf(x);
+            int floorNo = Integer.parseInt(floorNumber.getText());
+            int roomNo = Integer.parseInt(roomNumber.getText());
+            String aType = assetType.getText(); 
+            String err = AssetPlusFeatureSet3Controller.updateSpecificAsset(id, floorNo, roomNo, date, aType);
+            if (!err.isEmpty()) {
+                findAndUpdateEntry(assetID.getText(), String.valueOf(date), floorNumber.getText(), roomNumber.getText(), aType);
+                showError("Success!");
+                assetID.clear();
+                floorNumber.clear();
+                roomNumber.clear();
+                assetType.clear();
+            } else {
+                showError(err);
+            }
+        } catch (Exception e) {
+            showError("Invalid Parameters");
         }
     }
 
+    private void findAndUpdateEntry(String id, String date, String floor, String room, String assetType) {
+        int i = 0;
+        for (Asset asset: data) {
+            if (asset.getID().equals(id)) {
+                data.remove(i);
+                data.add(i, new Asset(id, date, floor, room, assetType));
+                break;
+            }
+            i++;
+        }
+        
+    }
+
+    @FXML
+    public void onDeleteBtnClick(ActionEvent event) {
+        try {
+            String no = textArea.getText();
+            int assetNumber = Integer.parseInt(no);
+            AssetPlusFeatureSet3Controller.deleteSpecificAsset(assetNumber);
+            textArea.clear();
+            boolean deleted = false;
+            for (Asset asset: data) {
+                if (no.equals(asset.getID())) {
+                    data.remove(asset);
+                    showError("Delete succesful!");
+                    deleted = true;
+                    break;
+                }
+            }
+            if (!deleted) {
+                showError("Could not find asset with id: " + assetNumber);
+            }
+        } catch (Exception e) {
+            showError("Invalid Input!");
+        }
+    }
 
 }
-
