90c6a3423f969064eb0c4ea12f408fc845532459
Seungyeon Lee <seungyeon.lee3@mail.mcgill.ca>
2023-11-30 21:08:14 -0500

    did maintenanceNote controller

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/MaintenanceNoteController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/MaintenanceNoteController.java
@@ -2,13 +2,17 @@ package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
 import static ca.mcgill.ecse.assetplus.javafx.fxml.controllers.ViewUtils.showError;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet4Controller;
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet7Controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -22,7 +26,7 @@ public class MaintenanceNoteController implements Initializable{
     private Button addButton;
 
     @FXML
-    private TextField dateInput;
+    private DatePicker dateInput;
 
     @FXML
     private Button deleteButton;
@@ -31,7 +35,7 @@ public class MaintenanceNoteController implements Initializable{
     private Button editButton;
 
     @FXML
-    private TableView<MaintenanceNote> notes;
+    private TableView<AMaintenanceNote> notes;
 
     @FXML
     private TextField noteAuthor;
@@ -43,20 +47,22 @@ public class MaintenanceNoteController implements Initializable{
     private TextField ticketInput;
 
     @FXML
-    private TableColumn<MaintenanceNote, Integer> ticketIDCol;
+    private TableColumn<AMaintenanceNote, Integer> ticketIDCol;
 
     @FXML
-    private TableColumn<MaintenanceNote, String> dateCol;
+    private TableColumn<AMaintenanceNote, String> dateCol;
 
     @FXML
-    private TableColumn<MaintenanceNote, String> descCol;
+    private TableColumn<AMaintenanceNote, String> descCol;
 
     @FXML
-    private TableColumn<MaintenanceNote, String> authorCol;
+    private TableColumn<AMaintenanceNote, String> authorCol;
 
-    private ObservableList<MaintenanceNote> data;
+    private ObservableList<AMaintenanceNote> data;
 
     private Integer ticketID;
+
+    private Integer current_id;
 
     public void setYourVariable(Integer value) {
         this.ticketID = value;
@@ -68,39 +74,102 @@ public class MaintenanceNoteController implements Initializable{
       try {
         String author = noteAuthor.getText();
         String description = noteDesc.getText();
-        Date date = Date.valueOf(dateInput.getText());
-        String tickeString = ticketInput.getText();
+        // Date date = Date.valueOf(dateInput.getValue());
+        Date date = Date.valueOf(dateInput.getValue());
+        String ticketString = ticketInput.getText();
+        System.out.println(ticketID);
+        String error = AssetPlusFeatureSet7Controller.addMaintenanceNote(date, description, ticketID, author);
+        
+        if (error.isEmpty()) {
+          data.add(new AMaintenanceNote(description, String.valueOf(date), author, current_id));
+          current_id++;
+          noteAuthor.clear();
+          noteDesc.clear();
+        } else {
+          showError(error);
+        }
 
       } catch (Exception e) {
         showError("Invalid parameters!");
       }
+      notes.refresh();
     }
 
     @FXML
     void deleteClick(ActionEvent event) {
+      int selectedID = notes.getSelectionModel().getSelectedIndex();
+      notes.getItems().remove(selectedID);
 
+      for (AMaintenanceNote ticket : data){
+          if (ticket.getNoteIndex() == selectedID) {
+              data.remove(ticket);
+          }
+      }
+
+      AssetPlusFeatureSet7Controller.deleteMaintenanceNote(ticketID, selectedID);
+      notes.refresh();
     }
 
     @FXML
     void editClick(ActionEvent event) {
+      try {
+        ObservableList<AMaintenanceNote> currentTableData = notes.getItems();
+        int currentTicketId = Integer.parseInt(ticketInput.getText());
 
+        for (AMaintenanceNote note : currentTableData) {
+            if (note.getNoteIndex() == currentTicketId) {
+                
+                Date date = Date.valueOf(dateInput.getValue());
+                String newDesc = noteDesc.getText();
+                String newEmail = noteAuthor.getText();
+                int noteIndex = Integer.parseInt(ticketInput.getText());
+
+                String error = AssetPlusFeatureSet7Controller.updateMaintenanceNote(ticketID, noteIndex, date, newDesc, newEmail);
+                if (error.isEmpty()) {
+                    note.setDate(String.valueOf(dateInput.getValue()));
+                    note.setNoteTaker(newEmail);
+                    note.setDescription(newDesc);
+                    notes.setItems(currentTableData);
+                    notes.refresh();
+                } else {
+                    showError(error);
+                }
+                break;
+            }
+        }
+    } catch (Exception e) {
+        showError("Invalid parameters!");
+    }
     }
 
     @FXML
     void rowClicked(MouseEvent event) {
-
+      AMaintenanceNote clickedTicket = notes.getSelectionModel().getSelectedItem();
+      ticketInput.setText(String.valueOf(clickedTicket.getNoteIndex()));
+      // dateInput.setText(String.valueOf(clickedTicket.getDate()));
+      noteAuthor.setText(String.valueOf(clickedTicket.getNoteTaker()));
+      noteDesc.setText(String.valueOf(clickedTicket.getDescription()));
+      
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
       // TODO Auto-generated method stub
-      ticketIDCol.setCellValueFactory(new PropertyValueFactory<MaintenanceNote, Integer>("ticketID"));
-      dateCol.setCellValueFactory(new PropertyValueFactory<MaintenanceNote, String>("date"));
-      authorCol.setCellValueFactory(new PropertyValueFactory<MaintenanceNote, String>("author"));
-      descCol.setCellValueFactory(new PropertyValueFactory<MaintenanceNote, String>("description"));
+      ticketIDCol.setCellValueFactory(new PropertyValueFactory<AMaintenanceNote, Integer>("noteIndex"));
+      dateCol.setCellValueFactory(new PropertyValueFactory<AMaintenanceNote, String>("date"));
+      authorCol.setCellValueFactory(new PropertyValueFactory<AMaintenanceNote, String>("noteTaker"));
+      descCol.setCellValueFactory(new PropertyValueFactory<AMaintenanceNote, String>("description"));
 
       data = FXCollections.observableArrayList();
+      List<AMaintenanceNote> myList = AssetPlusFeatureSet7Controller.getSpecificNotes(ticketID);
+      System.out.println("TICket id is: " + ticketID);
+      for (AMaintenanceNote note : myList){
+        data.add(note);
+        current_id = note.getNoteIndex() + 1;
+        System.out.println("done");
+      }
+      
       notes.setItems(data);
-
+      notes.refresh();
       System.out.println(ticketID);
     }
 
